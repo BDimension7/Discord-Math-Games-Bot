a565262b973ce17de40f2130d95f4d1b4eb45423
BDimension7 <54754524+BDimension7@users.noreply.github.com>
2022-05-16 18:01:07 -0400

    Delete freap.js

--- commands/freap.js
+++
@@ -1,7 +0,0 @@
-const Database = require("@replit/database")
-const db = new Database()
-
-module.exports.run = (client, msg, args) => {
-    msg.channel.send("Not available currently.")
-}
-exports.name = "freap"
